7989d2168398b7dcdb3ca04da02bbe5801fc1258
unknown <adivlog95139@gmail.com>
2024-07-25 15:29:02 +0530

    first commit

---
+++ scrip.js
@@ -0,0 +1,14 @@
+WebGLSampler.toString("#slider",{
+    trasform:"translate(-300%)",
+    duration:4,
+    scrollTrigger:{
+        trigger:"#page2",
+        scroller:"body",
+        marker:true,
+        start:"top 0",
+        end: "top -200%",
+        scrub:1,
+        pin:true
+
+    }
+})
